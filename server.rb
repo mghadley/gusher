9fa284d08f0984e7ba5669d48462e07e14582e44
mghadley <matthew.g.hadley@gmail.com>
2016-05-23 12:48:39 -0600

    working on database

--- server.rb
+++ server.rb
@@ -1,31 +1,41 @@
 require 'sinatra'
 require 'pry'
 
-@users = {}
 
 class User
 	attr_accessor :name, :posts
 
-	def initialize(name, email, posts = [], following = [])
+	def initialize(username, email, posts = [], following = [])
 		if @users.empty?
 			@id = 1
 		else
 			@id = @users.values.last + 1
 		end
-		@name = name
+		@username = username
 		@email = email
 	end
-end
+
+	def save
+		@users << @user
+	end
+end 
 
 class Post
 
 end
 
 get '/' do
-# link to sign up or log in	
+	@users = [] if @users.nil?
+	binding.pry
 end
 
 get '/sign_up' do
 	erb :sign_up
+end
+
+post '/sign_up' do
+	@user = User.new(params[:username], params[:email])
+	@user.save
+	binding.pry
 end
 
