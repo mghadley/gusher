c4de5c2cdd398ba3cf382218fcb1f7fff4f7ede3
mghadley <matthew.g.hadley@gmail.com>
2016-05-23 11:59:38 -0600

    working on user class

--- server.rb
+++ server.rb
@@ -1,8 +1,31 @@
-class User
-	attr_accessor
+require 'sinatra'
+require 'pry'
 
-	def initialize(name)
+@users = {}
+
+class User
+	attr_accessor :name, :posts
+
+	def initialize(name, email, posts = [], following = [])
+		if @users.empty?
+			@id = 1
+		else
+			@id = @users.values.last + 1
+		end
 		@name = name
+		@email = email
 	end
+end
+
+class Post
+
+end
+
+get '/' do
+# link to sign up or log in	
+end
+
+get '/sign_up' do
+	erb :sign_up
 end
 
