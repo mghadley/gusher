a2e35e9d920346f70eb6bda0237c9a881cc31313
mghadley <matthew.g.hadley@gmail.com>
2016-05-23 14:11:59 -0600

    starting header

--- server.rb
+++ server.rb
@@ -3,13 +3,19 @@ require 'pry'
 
 
 class User
-	attr_accessor :username, :email, :posts, :following
+	attr_accessor :username, :email, :password, :posts, :following, :id
 
-	def initialize(username, email, posts = [], following = [])
+	def initialize(username, email, password, posts = [], following = [])
 		@username = username
 		@email = email
+		@password = password
 		@posts = posts
 		@following = following
+		if $users.empty?
+			@id = 1
+		else
+			@id = $users.last.id + 1
+		end
 	end
 end 
 
@@ -17,20 +23,26 @@ $users = []
 $current_user = ''
 
 get '/' do
-	binding.pry
+	redirect to ('/sign_up') if $current_user == ''
+	erb :home
 end
 
 get '/sign_up' do
 	erb :sign_up
 end
 
+get '/log_in' do
+
+end
+
+
+
 post '/sign_up' do
 	binding.pry
-	@user = User.new(params[:username], params[:email])
+	@user = User.new(params[:username], params[:email], params[:password])
 	$users << @user
 	$current_user = @user
 	binding.pry
 end
 
-post
 
