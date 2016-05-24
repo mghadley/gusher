db2c27e3b81c9f1e37f5b22f6fbda59d3c456dee
Mara Behles Dean <maraebehles@gmail.com>
2016-05-24 09:17:22 -0600

    actual final changes

--- server.rb
+++ server.rb
@@ -14,7 +14,7 @@ $users << @beyonce
 									Time.now.usec => "Can't wait to move out of this dump.", Time.now.usec => "Who is Becky with the good hari??"})
 $users << @obama
 
-@jakesorce = User.new("Jake_Sorce", "jakes@ridingthegnar.com", "password",
+@jakesorce = User.new("Jake Sorce", "jakes@ridingthegnar.com", "password",
 					{Time.now.usec => "Code4life", Time.now.usec => "Shreddin' the fresh pow", Time.now.usec => "Need a nap right MEOWWW", Time.now.usec => "Beer or Break"})
 $users << @jakesorce
 
